ea6c4c8353f8d759cede29eaac74b2a13640004b
Vicki Edwards <vicki.edwards@quadrotech-it.com>
2020-11-30 18:37:50 +0000

    Styling for main layout

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -1,18 +1,67 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
+import Logo from './Logo';
+
+const StyledNav = styled.nav`
+    margin-bottom: 3rem;
+    ul {
+        margin: 0;
+        margin-top: -6rem;
+        padding: 0;
+        display: grid;
+        grid-template-columns: 1fr 1fr auto 1fr 1fr;
+        grid-gap: 2rem;
+        text-align: center;
+        list-style: none;
+        align-items: center;
+    }
+    li {
+        --rotate: -2deg;
+        transform: rotate(var(--rotate));
+        order: 1;
+        &:nth-child(1) {
+            --rotate: 1deg;
+        }
+        &:nth-child(2) {
+            --rotate: 2.5deg;
+        }
+        &:nth-child(4) {
+            --rotate: 2.5deg;
+        }
+        &:hover {
+            --rotate: 3deg;
+        }
+    }
+    a {
+        font-size: 3 rem;
+        text-decoration: none;
+        &:hover {
+            color: var(--red);
+        }
+        &[aria-current="page"] {
+            color: var(--red);
+        }
+    }
+    .logo {
+        transform: translateY(-25%);
+    }
+`;
 
 export default function Nav() {
     return (
-        <nav>
+        <StyledNav>
             <ul>
                 <li>
                     <Link to="/">Hot now</Link>
                 </li>
                 <li>
-                    <Link to="/">LOGO</Link>
+                    <Link to="/pizza">Pizza Menu</Link>
                 </li>
                 <li>
-                    <Link to="/pizza">Pizza Menu</Link>
+                    <Link to="/">
+                        <Logo />
+                    </Link>
                 </li>
                 <li>
                     <Link to="/sliceMasters">Slice Masters</Link>
@@ -21,6 +70,6 @@ export default function Nav() {
                     <Link to="/order">Order Ahead</Link>
                 </li>
             </ul>
-        </nav>
+        </StyledNav>
     )
 }
