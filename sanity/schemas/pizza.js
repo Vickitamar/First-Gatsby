b66f6dddc6be38de2760cff41881acb29be37a7d
Vicki Edwards <vicki.edwards@quadrotech-it.com>
2020-12-22 14:05:12 +0000

    pizza schema sainty

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -1,4 +1,5 @@
 import { MdLocalPizza as icon } from 'react-icons/md';
+import PriceInput from '../components/PriceInput';
 export default {
     name: 'pizza',
     title: 'Pizzas',
@@ -32,8 +33,36 @@ export default {
             name: 'price',
             title: 'Price',
             type: 'number',
+            inputComponent: PriceInput,
             description: 'Price of the pizza',
             validation: Rule => Rule.min(1000),
         },
+        {
+            name: 'toppings',
+            title: 'Toppings',
+            type: 'array',
+            of: [{ type: 'reference', to: [{ type: 'topping' }] }]
+        },
     ],
+    preview: {
+        select: {
+            title: 'name',
+            media: 'image',
+            topping0: 'toppings.0.name',
+            topping1: 'toppings.1.name',
+            topping2: 'toppings.2.name',
+            topping3: 'toppings.3.name',
+        },
+        prepare: ({ title, media, ...toppings}) => {
+            // filter undefined toppings out
+            const tops = Object.values(toppings).filter(Boolean);
+            //return the preview object for the pizza
+            return {
+                title,
+                media,
+                subtitle: tops.join(', '),
+            }
+
+        }
+    }
 }
